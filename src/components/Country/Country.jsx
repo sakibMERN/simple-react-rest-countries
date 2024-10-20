085a467e8bc9bf4ba73fc935e06425e0ba6349db
sakibMERN <sakib.sr57@gmail.com>
2024-10-20 18:14:01 +0600

    make a countries component and load data from api

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Country() {
+  return (
+    <div>Country</div>
+  )
+}
---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useState } from "react"
+
+export default function Countries() {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    }, [])
+  return (
+    <div>
+        <h3>Countries: {countries.length}</h3>
+    </div>
+  )
+}
