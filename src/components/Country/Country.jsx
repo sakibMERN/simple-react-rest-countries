cceeac619b52d22410dd22063a92cd7afea2fd66
sakibMERN <sakib.sr57@gmail.com>
2024-10-21 09:34:16 +0600

    add dynamic toggle button for showing visited status

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "./Country.css"
 export default function Country({country}) {
     console.log(country);
-    const {name, flags, area, population} = country;
+    const {name, flags, area, population, cca3} = country;
+
+    const [visited , setVisited] = useState(false);
+
+    const handleVisited = () => {
+      setVisited(!visited);
+    }
   return (
     <div className="country">
       <h2>{name.common}</h2>
       <img src={flags.png} alt="" />
       <p>Population: {population} </p>
       <p>Area: {area} square kilometer </p>
+      <p><small>Code: {cca3} </small></p>
+      <button onClick={handleVisited}>{visited ? "Visited" : "Want to Go"}</button>
+      <br />
+      {visited && "I have visited this country."}
     </div>
   )
 }
