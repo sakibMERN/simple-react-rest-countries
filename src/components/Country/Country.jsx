4698105f949c0aa3d82b9845417b4466b670f013
sakibMERN <sakib.sr57@gmail.com>
2024-10-21 12:17:58 +0600

    add Mark Visited button and add event handlar

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Country.css"
-export default function Country({country}) {
+export default function Country({country, handleVisitedCountry, handleVisitedFlags}) {
     console.log(country);
     const {name, flags, area, population, cca3} = country;
 
@@ -9,14 +9,20 @@ export default function Country({country}) {
     const handleVisited = () => {
       setVisited(!visited);
     }
+    
   return (
-    <div className="country">
-      <h2>{name.common}</h2>
+    <div className={`country ${visited && "visited"}`}>
+      <h2 style={{color: visited ? "purple": "white"}}>{name.common}</h2>
       <img src={flags.png} alt="" />
       <p>Population: {population} </p>
       <p>Area: {area} square kilometer </p>
       <p><small>Code: {cca3} </small></p>
-      <button onClick={handleVisited}>{visited ? "Visited" : "Want to Go"}</button>
+
+      <button onClick={()=>handleVisitedCountry(country)}>Mark Visited</button>
+      <br />
+      <button onClick={handleVisitedFlags}>Add Flag</button>
+      <br />
+      <button style={{margin: "10px"}} onClick={handleVisited}>{visited ? "Visited" : "Want to Go"}</button>
       <br />
       {visited && "I have visited this country."}
     </div>
