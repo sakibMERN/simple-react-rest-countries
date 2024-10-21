13a9f6cc8051b64b95f67fdd40b774c3946f5624
sakibMERN <sakib.sr57@gmail.com>
2024-10-21 09:11:36 +0600

    add dynamic population and area inside Country component

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,13 @@
-
+import "./Country.css"
 export default function Country({country}) {
     console.log(country);
+    const {name, flags, area, population} = country;
   return (
-    <div>Country</div>
+    <div className="country">
+      <h2>{name.common}</h2>
+      <img src={flags.png} alt="" />
+      <p>Population: {population} </p>
+      <p>Area: {area} square kilometer </p>
+    </div>
   )
 }
